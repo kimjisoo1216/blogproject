82c896ae177fd7a757c64437d0b6e2a39869b2e7
kimjisoo <kimjisoo1216@naver.com>
2019-03-07 23:43:19 +0900

    accout add

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'portfolio.apps.PortfolioConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
